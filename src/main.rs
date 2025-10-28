da13822f47bd0e00eaac3f00b10321bdbe2f813b
dylan0804 <dylan.halim@binus.ac.id>
2025-10-28 22:08:25 +0700

    test port 443

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use libp2p::{
 };
 use std::{error::Error, fs, net::Ipv4Addr, path::Path};
 
-const PORT: u16 = 4001;
+const PORT: u16 = 443;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
